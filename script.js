6dbcb83aaa62a7612433231da21c394b78c41990
Shahla Maharramova <78307910+ShahlaMva9@users.noreply.github.com>
2022-09-07 14:44:02 +0400

    Throw error manually

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const tbody = document.querySelector("tbody");
-
+const table = document.querySelector(".table");
 const getPosts = () => {
   fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Posts not found (${response.status})`);
+      }
+      return response.json();
+    })
     .then((data) => {
       renderPosts(data);
     })
     .catch((err) => {
+      table.innerHTML = `
+      <h1>${err.message}</h1>
+      `;
       console.log("Error: ", err.message);
     })
     .finally(() => {
@@ -29,3 +38,6 @@ function renderPosts(post) {
 }
 
 getPosts();
+
+const arr = [1, 2];
+arr.length;
