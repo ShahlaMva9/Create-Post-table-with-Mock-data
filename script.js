478e3c1fa08129007928b670efe68bd5392ddc76
Shahla Maharramova <78307910+ShahlaMva9@users.noreply.github.com>
2022-09-07 14:18:24 +0400

    Add Catch and finally

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const tbody = document.querySelector("tbody");
 
-function renderPosts(post) {
-  post.forEach((el) => {
-    const tbodyContent = `
-    <tr>
-            <td>${el.id}</td>
-            <td>${el.userId}</td>
-            <td>${el.title}</td>
-    </tr>
-    `;
-    tbody.innerHTML += tbodyContent;
-  });
-}
-
-const getPosts = (post) => {
+const getPosts = () => {
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((data) => {
       renderPosts(data);
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
+    })
+    .finally(() => {
+      console.log("Finally");
     });
 };
+function renderPosts(post) {
+  post.forEach((el) => {
+    const tbodyContent = `
+      <tr>
+              <td>${el.id}</td>
+              <td>${el.userId}</td>
+              <td>${el.title}</td>
+              <td>${el.body}</td>
+  
+      </tr>
+      `;
+    tbody.innerHTML += tbodyContent;
+  });
+}
 
 getPosts();
